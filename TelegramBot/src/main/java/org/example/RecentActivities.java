cd460e08440d7c0e14d5c8a306e44e127ba96aa9
AmielCohen96 <117925491+AmielCohen96@users.noreply.github.com>
2023-06-27 23:12:58 +0300

    FINAL

--- TelegramBot/src/main/java/org/example/RecentActivities.java
+++ TelegramBot/src/main/java/org/example/RecentActivities.java
@@ -8,8 +8,8 @@ public class RecentActivities extends JPanel {
     public RecentActivities(){
         setBorder(BorderFactory.createTitledBorder("RecentActivities"));
         setLayout(null);
-        activity.setBounds(25, 0, 600, 400);
-        activity.setFont(new Font("David", Font.PLAIN, 30));
+        activity.setBounds(Constant.X_START, 0, 600, 400);
+        activity.setFont(new Font("David", Font.PLAIN, Constant.FONT_SIZE - 10));
         add(activity);
     }
 
